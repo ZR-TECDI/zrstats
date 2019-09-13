1df21fb0b65ba421dcae41e85b17ad9f5b9072e3
Andrés Riquelme <andresfranco.rs@gmail.com>
2019-09-13 20:06:21 -0300

    refactor(models): wip datos_iniciales incluye miembros

--- datos_iniciales.py
+++ datos_iniciales.py
@@ -1,13 +1,19 @@
 """Agrega los datos iniciales de forma automática"""
 
 from asistencia.models import *
+from collections import defaultdict
+import os
+
+lista = [Unidad, Rango, Rol, Clase, Nacionalidad, Miembro]
+for item in lista:
+    item.objects.all().delete()
 
 
 def crear_unidades():
     unidades_dict = {
         'altm': ['Alto Mando', 'ALTM'],
         'infm': ['Inf. de Marines', 'IMZR'],
-        'paraca': ['Paracaidistas', 'PARACA'],
+        'paraca': ['Paracaidistas', 'PAR'],
         'echo': ['Espectro', 'ECHO'],
         'cab': ['Caballería', 'CAB'],
         'fazr': ['Fuerza Aérea', 'FAZR'],
@@ -55,7 +61,13 @@ def crear_rol():
         'cmd': ['Comandante Unidad', 'CMD'],
         'ce': ['Comandante Escuadra', 'CE'],
         'lea': ['Líder Equipo A', 'LEA'],
-        'leb': ['Líder Equipo B', 'LEB']
+        'leb': ['Líder Equipo B', 'LEB'],
+        'a':['ALPHA', 'A'],
+        'b':['BRAVO', 'B'],
+        'cmda1':['Com. Ariete 1', 'CMDA1'],
+        'cmda2':['Com. Ariete 2', 'CMDA2'],
+        'cmda3':['Com. Ariete 3', 'CMDA3'],
+        'cmda4':['Com. Ariete 4', 'CMDA4']
     }
 
     print('Creando roles...')
@@ -94,7 +106,7 @@ def crear_naciones():
     naciones_dict = {
         'arg': ['Argentina', 'ARG'],
         'cl': ['Chile', 'CL'],
-        'col': ['Colombia', 'COL'],
+        'co': ['Colombia', 'CO'],
         'cr': ['Costa Rica', 'CR'],
         'sv': ['El Salvador', 'SV'],
         'jp': ['Japón', 'JP'],
@@ -106,7 +118,8 @@ def crear_naciones():
         've': ['Venezuela', 'VE'],
         'pr': ['Puerto Rico', 'PR'],
         'bo': ['Bolivia', 'BO'],
-        'rd': ['República Dominicana', 'RD']
+        'rd': ['República Dominicana', 'RD'],
+        'ca': ['Canadá', 'CA']
     }
 
     print('Creando naciones...')
@@ -117,6 +130,50 @@ def crear_naciones():
         var.abreviatura = array[1]
         var.save()
 
+def agregar_miembros():
+    #rango|nombre|clase1|clase2|nac|est|unidad|pel|esc|rol
+    # 0      1      2     3      4   5     6    7   8   9
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    datos_iniciales = script_dir + '\\datos_iniciales.txt'
+    with open(datos_iniciales) as txt:
+        for line in txt.readlines():
+            array = line.replace('\n', '').split(' ')
+
+            blabla = Rango.objects.get(abreviatura__iexact= array[0])
+            print (blabla)
+
+            print(array)
+            miembro = Miembro.objects.create()
+            miembro.nombre = array[1]
+            print(miembro.nombre)
+            # miembro.rango = Rango.objects.get(abreviatura__iexact= array[0])
+            miembro.rango = Rango.objects.get(abreviatura = 'Cap')
+            print(miembro.rango)
+            miembro.clase1 = Clase.objects.get(abreviatura = array[2])
+            print(miembro.clase1)
+            miembro.clase2 = Clase.objects.get(abreviatura = array[3])
+            print(miembro.clase2)
+            miembro.nacionalidad = Nacionalidad.objects.get(abreviatura = array[4])
+
+            ESTADO_CHOICES = (
+            ('Activo', 'Activo'),
+            ('Reserva', 'Reserva'),
+            ('Licencia', 'Licencia'),
+            ('No Miembro', 'No Miembro'),
+            )
+            if array[5] == 'A':
+                el_estado = ESTADO_CHOICES[0]
+            elif array[5] == 'R':
+                el_estado = ESTADO_CHOICES[1]
+            miembro.estado = el_estado
+
+            miembro.unidad = Unidad.objects.get(abreviatura = array[6])
+            miembro.peloton = array[7]
+            miembro.escuadra = array[8]
+            miembro.rol = Rol.objects.get(abreviatura = array[9])
+
+            miembro.save()
 
 def main():
     crear_unidades()
@@ -124,7 +181,7 @@ def main():
     crear_rol()
     crear_clase()
     crear_naciones()
-
+    agregar_miembros()
 
 if __name__ == '__main__':
     main()
