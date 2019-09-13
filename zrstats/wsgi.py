f5b8180b80a4fb7b447597e62c002be5fb25e78c
Cyberpunx <franco.mgr@gmail.com>
2019-09-13 17:10:18 -0300

    feat(models): Agregado en wsgi.py el llamado a datos_iniciales.py

--- zrstats/wsgi.py
+++ zrstats/wsgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zrstats.settings')
 
 application = get_wsgi_application()
+
+# Cambiar primera_vez a True para cargar los datos iniciales en la Base de Datos
+# 1) primera_vez = True.
+# 2) Ejecutar Django, dejar que carguen Unidades, Rangos, Clases, Paises, etc..
+# 3) Detener Django y volver a poner primera_vez = False
+# 4) profit
+primera_vez = False
+
+if primera_vez:
+    import datos_iniciales
+    datos_iniciales.main()
+
