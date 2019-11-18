6b62f3649e4d0e3690e0af443699d2a96a86de13
Andrés Riquelme <andresfranco.rs@gmail.com>
2019-11-18 19:29:34 -0300

    feat(pipeline): sistema de build para el live server

---
+++ builder.py
@@ -0,0 +1,60 @@
+"""Construye y despliega la aplicación en nuestro servidor live"""
+
+import os
+import spur
+
+# Constantes
+LIVE_USER = os.environ['LIVE_USER']
+LIVE_HOST = os.environ['LIVE_HOST']
+LIVE_PASS = os.environ['LIVE_PASS']
+APP_PATH = os.environ['APP_PATH']
+
+STOP = ['systemctl', 'stop', 'gunicorn']
+PULL = ['git', 'pull', 'origin', 'master']
+START = ['systemctl', 'start', 'gunicorn']
+
+print('************************************************************')
+print('Comenzando el despliegue del sitio en el servidor live: ')
+
+def stop():
+    shell = spur.SshShell(hostname=LIVE_HOST, username=LIVE_USER, password=LIVE_PASS)
+    with shell:
+        print('Deteniendo el servicio gunicorn...')
+        result = shell.run(STOP, cwd=APP_PATH)
+        result = str(result.output)
+        result = result.split('\n')
+
+    for line in result:
+        print(line)
+    return 0
+
+def pull():
+    shell = spur.SshShell(hostname=LIVE_HOST, username=LIVE_USER, password=LIVE_PASS)
+    with shell:
+        print('Pulleando últimos cambios desde master...')
+        result = shell.run(PULL, cwd=APP_PATH)
+        result = str(result.output)
+        result = result.split('\n')
+
+    for line in result:
+        print(line)
+    return 0
+
+def start():
+    print('Comenzando el servicio una vez más...')
+    shell = spur.SshShell(hostname=LIVE_HOST, username=LIVE_USER, password=LIVE_PASS)
+    with shell:
+        result = shell.run(START, cwd=APP_PATH)
+        result = str(result.output)
+        result = result.split('\n')
+
+    for line in result:
+        print(line)
+    return 0  
+
+if __name__ == '__main__':
+    stop()
+    pull()
+    start()
+    print('Despliegue del sitio en servidor live terminado')
+    print('************************************************************')
